b41fc2aa20572e6b7bd2bdd282ae537c47fc55ea
synj <matthew.gregory@hotmail.co.uk>
2018-04-16 16:55:44 +0100

    work in progress

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,30 @@
-// check that JavaScript is enabled ;)
-1 + 1 == 2
+var popupS = require('popups');
+var ls = localStorage.getItem('namespace.visited');
+
+if (ls == null) {
+  popupS.modal({
+      content: `<!-- /43271057/AAA_Static_interstitial -->
+<div id='div-gpt-ad-1523874209243-0'>
+  <script>
+    googletag.cmd.push(function() {
+      googletag.display('div-gpt-ad-1523874209243-0');
+    });
+  </script>
+</div>`
+  });
+  localStorage.setItem( 'namespace.visited', 1 )
+}
+
+// if #div-gpt-ad-1523874209243-0 hasn't propetty 'display: none'
+
+// var dfpContent = document.getElementById("div-gpt-ad-1523874209243-0");
+// var visible = window.getComputedStyle(dfpContent, null).getPropertyValue("display")
+
+isHidden = el => {
+  var style = window.getComputedStyle(el);
+  return (style.display === 'none')
+};
+
+window.onload = function() {
+  console.log(isHidden(document.getElementById('div-gpt-ad-1523874209243-0')));
+}
