baacc59aef034a1cf9140532506c97ae761543c9
synj <matthew.gregory@hotmail.co.uk>
2018-04-17 17:35:18 +0100

    display splash popup with content from dfp

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,12 @@
 var popupS = require('popups');
-var ls = localStorage.getItem('namespace.visited');
 
-if (ls == null) {
-  popupS.modal({
-      content: `<!-- /43271057/AAA_Static_interstitial -->
+popupS.modal({
+    content: `<!-- /43271057/AAA_Static_interstitial -->
 <div id='div-gpt-ad-1523874209243-0'>
-  <script>
-    googletag.cmd.push(function() {
-      googletag.display('div-gpt-ad-1523874209243-0');
-    });
-  </script>
-</div>`
+<script>
+  googletag.cmd.push(function() {
+    googletag.display('div-gpt-ad-1523874209243-0');
   });
-  localStorage.setItem( 'namespace.visited', 1 )
-}
-
-// if #div-gpt-ad-1523874209243-0 hasn't propetty 'display: none'
-
-// var dfpContent = document.getElementById("div-gpt-ad-1523874209243-0");
-// var visible = window.getComputedStyle(dfpContent, null).getPropertyValue("display")
-
-isHidden = el => {
-  var style = window.getComputedStyle(el);
-  return (style.display === 'none')
-};
-
-window.onload = function() {
-  console.log(isHidden(document.getElementById('div-gpt-ad-1523874209243-0')));
-}
+</script>
+</div>`
+});
