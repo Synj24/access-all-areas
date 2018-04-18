a01f2be4e77fb105b6a1b749cbebcb91d4915036
synj <matthew.gregory@hotmail.co.uk>
2018-04-18 11:49:20 +0100

    only appear when a new visitor arrives

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,30 @@
 var popupS = require('popups');
 
-popupS.modal({
-    content: `<!-- /43271057/AAA_Static_interstitial -->
+// [x] should appear only when a new visitor arrives
+// [ ] should not appear when there is no order in dfp
+displaySplash = () => {
+  popupS.modal({
+    content:
+`<!-- /43271057/AAA_Static_interstitial -->
 <div id='div-gpt-ad-1523874209243-0'>
-<script>
-  googletag.cmd.push(function() {
-    googletag.display('div-gpt-ad-1523874209243-0');
-  });
-</script>
+  <script>
+    googletag.cmd.push(function() {
+      googletag.display('div-gpt-ad-1523874209243-0');
+    });
+  </script>
 </div>`
-});
+  });
+}
+
+firstVisit = () => {
+  var ls = localStorage.getItem('namespace.visited');
+
+  if (ls == null) {
+    localStorage.setItem( 'namespace.visited', 1 );
+    return true
+  } else {
+    return false
+  }
+}
+
+if (firstVisit()) displaySplash();
